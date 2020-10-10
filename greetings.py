36c9392aaccc0dd4c63d1422409f70ada6847877
CruzHenry56 <henry.cruz@mail.citytech.cuny.edu>
2020-10-10 14:08:11 -0400

    lab

---
+++ greetings.py
@@ -0,0 +1,7 @@
+var myVariable = someValue;
+myVariable = someValue
+
+username = input("So, like, what's your name? ")
+
+print("Look Mom, no semicolon!")
+print("Hi, " + username + "!")
---
+++ madlibs.py
@@ -0,0 +1,6 @@
+noun = input("Enter a noun.");
+adjective = input("Enter a adjective.")
+adverb = input ("Enter a adverb")
+verb = input("Enter a verb");
+
+print("On a tropical "+ noun +"underneath a "+adjective + "underneath a " + adverb + verb);
